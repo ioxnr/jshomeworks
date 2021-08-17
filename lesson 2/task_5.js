9521a7df177d814bdfc20045efec2e7960634ff1
ioxnr <lionasalvia@gmail.com>
2021-08-17 16:31:19 +0300

    added lesson 2 tasks

---
+++ lesson 2/task_5.js
@@ -0,0 +1,20 @@
+function addition(a, b) {
+    return a + b;
+}
+
+function subtraction(a, b) {
+    return a - b;
+}
+
+function multiplication(a, b) {
+    return a * b;
+}
+
+function division(a, b) {
+    return a / b;
+}
+
+console.log(addition(3, 6));
+console.log(subtraction(3, 6));
+console.log(multiplication(3, 6));
+console.log(division(3, 6));
